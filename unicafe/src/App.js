a777998e4586730affd9fec8f8f80b707fde4a72
Eddy Ding <edwarding04@gmail.com>
2023-07-02 19:14:31 -0400

    Complete 1.6

---
+++ unicafe/src/App.js
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick = {props.action} >
+    {props.text}
+  </button>
+)
+
+const Display = (props) => (
+  <div>{props.text} {props.value}</div>
+)
+
+function App() {
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const incrementValue = (value, setFunction) => {
+    setFunction(value + 1);
+  }
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      
+      <Button action= {() => incrementValue(good, setGood)} text="good"/>
+      <Button action= {() => incrementValue(neutral, setNeutral)} text="neutral"/>
+      <Button action= {() => incrementValue(bad, setBad)} text="bad"/>
+
+      <h1>statistics</h1>
+      <Display text="good" value={good} />
+      <Display text="neutral" value={neutral} />
+      <Display text="bad" value={bad} />
+    </>
+
+  );
+}
+
+export default App;
