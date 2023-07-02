946a7d55a45be3f269c9355e04e27d0f1ac870d9
Eddy Ding <edwarding04@gmail.com>
2023-07-02 19:18:08 -0400

    Complete 1.7

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -15,7 +15,7 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
+  const total = good + neutral + bad;
   const incrementValue = (value, setFunction) => {
     setFunction(value + 1);
   }
@@ -32,6 +32,9 @@ function App() {
       <Display text="good" value={good} />
       <Display text="neutral" value={neutral} />
       <Display text="bad" value={bad} />
+      <Display text="all" value= {total}/>
+      <Display text="average" value= {(good - bad) / total}/>
+      <Display text="postive" value= {`${good / total}%`}/>
     </>
 
   );
